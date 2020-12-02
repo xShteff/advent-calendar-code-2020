8f61be229794ee64b6a95160ca7d157668fcf9b7
Alin Stefan Olaru <alinstefanolaru@gmail.com>
2020-12-02 22:40:19 +0100

    day 2 half done

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,33 +1,54 @@
 import { readFileSync } from "fs";
 
 interface PasswordEntry {
-    characterMin: number;
-    characterMax: number;
-    character: string;
-    password: string;
+  characterMin: number;
+  characterMax: number;
+  character: string;
+  passwordString: string;
 }
 
+const characterCountInString = function(
+  character: string,
+  input: string
+): number {
+  const regex = new RegExp(character, "g");
+  return input.match(regex)?.length || 0;
+};
 
 let data: PasswordEntry[] = [];
 
-// 10-12 r: rrrrgdrrrzrs
-const regex: RegExp = /(\d\d|\d)-(\d\d|\d) (\w): (\w*)/g;
-regex.lastIndex = 1;
 try {
-    let fileData: string[] = readFileSync('./inputs/day2.txt', 'utf8').split('\n');
-    fileData.map((i) => {
-        let iArray = regex.exec(i);
-        if(iArray) {
-            data.push({
-                character: iArray[3],
-                characterMin: Number(iArray[1]),
-                characterMax: Number(iArray[2]),
-                password: iArray[4]
-            })
-        }
-    })
-} catch(e) {
-    console.log('Error:', e.stack);
+  let fileData: string[] = readFileSync("./inputs/day2.txt", "utf8").split(
+    "\n"
+  );
+  for (let i = 0; i < fileData.length; i++) {
+    let iArray = /(\d\d|\d)-(\d\d|\d) (\w): (\w*)/g.exec(fileData[i]);
+    if (iArray) {
+      data.push({
+        character: iArray[3],
+        characterMin: Number(iArray[1]),
+        characterMax: Number(iArray[2]),
+        passwordString: iArray[4],
+      });
+    }
+  }
+} catch (e) {
+  console.log("Error:", e.stack);
 }
 
-console.log(data);
+let incorrectPasswordCount = 0;
+
+data.map((password) => {
+  const charCount = characterCountInString(
+    password.character,
+    password.passwordString
+  );
+  if (
+    charCount >= password.characterMin &&
+    charCount <= password.characterMax
+  ) {
+    incorrectPasswordCount++;
+  }
+});
+
+console.log(incorrectPasswordCount);
